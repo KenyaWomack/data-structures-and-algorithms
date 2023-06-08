fd11770c8e629b4afcc265f8ba5327d46b2c4285
Kenya Womack <ladykamillion27@gmail.com>
2023-06-08 16:55:05 -0500

    code challenge 18

---
+++ javascript/tree-fizz-buzz/fizz-buzz.test.js
@@ -0,0 +1,10 @@
+const kAryTree = require("./k-ary-tree");
+const { Node } = require("./fizz-buzz");
+/*
+var tree = new kAryTree(1);
+tree.root.children.push(new Node(3));
+tree.root.children[0].parent = tree;
+console.log("before", tree.preOrder());
+let newTree = fizzBuzz(tree);
+console.log("after", newTree.preOrder());
+*/
